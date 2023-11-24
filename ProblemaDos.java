2a0c03852fc28df0f6a5ff8e46eb2e0727997bc3
Pablofh97 <pablofajardoherrera@gmail.com>
2023-11-24 19:22:56 +0100

    version1

---
+++ ProblemaDos.java
@@ -0,0 +1,10 @@
+public class ProblemaDos{
+    public static void main(String[] args) {
+        System.out.println(" Git es un sistema de controlador de versiones de tipo local o de tipo distribuido.");
+        System.out.println("Una de las principales ventajes es que nos permite tener versiones de distintos proyectos y poder adceder a ellos.");
+        System.out.println("Se basa en repositorios y pueden ser locales o compartidos.");
+        System.out.println("La principal ventaja de ser distribuido es que si se pierde el contenido se puede recuperar de cualquier cliente que tenga el repositorio o del servidor.");
+        System.out.println("Los repositorios se pueden clonar o hacer fork, esta ultima es totalmente independiente de la original.");
+    }
+
+}
